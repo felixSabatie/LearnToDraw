1dee0c367bf62f053529e19331cafa8413480331
Unknown <felix.sabatie@gmail.com>
2018-10-16 09:11:14 -0400

    WIP

---
+++ app/src/main/java/com/sabatie/felix/learntodraw/fragments/ResponseDialog.kt
@@ -0,0 +1,21 @@
+package com.sabatie.felix.learntodraw.fragments
+
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.sabatie.felix.learntodraw.R
+
+class ResponseDialog : DialogFragment() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = 
+    }
+}
