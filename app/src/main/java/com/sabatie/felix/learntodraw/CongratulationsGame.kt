0eaf7f849af92428b35fbc4403f30818977a4446
Unknown <felix.sabatie@gmail.com>
2018-10-17 14:02:55 -0400

    Save progress in shared preferences

--- app/src/main/java/com/sabatie/felix/learntodraw/CongratulationsGame.kt
+++ app/src/main/java/com/sabatie/felix/learntodraw/CongratulationsGame.kt
@@ -1,5 +1,6 @@
 package com.sabatie.felix.learntodraw
 
+import android.content.Context
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
@@ -13,6 +14,13 @@ class CongratulationsGame : AppCompatActivity() {
         supportActionBar?.hide()
         (CurrentGame.gamesList.find { it.name == CurrentGame.game!!.name } as Game).completed = true
         setContentView(R.layout.activity_congratulations_game)
+
+        saveProgress()
+    }
+
+    private fun saveProgress() {
+        val preferences = this.getSharedPreferences(getString(R.string.preferences_file), Context.MODE_PRIVATE) ?: return
+        preferences.edit().putBoolean("${CurrentGame.game!!.name}_completed", true).apply()
     }
 
     override fun onBackPressed() {
--- app/src/main/java/com/sabatie/felix/learntodraw/MainActivity.kt
+++ app/src/main/java/com/sabatie/felix/learntodraw/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.sabatie.felix.learntodraw
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
+import android.widget.Toast
 import com.sabatie.felix.learntodraw.game.GameFactory
 
 
@@ -18,6 +20,10 @@ class MainActivity : AppCompatActivity() {
 
     fun startGame(v: View) {
         CurrentGame.gamesList = GameFactory.createGames()
+        val preferences = this.getSharedPreferences(getString(R.string.preferences_file), Context.MODE_PRIVATE) ?: return
+        CurrentGame.gamesList.forEach {
+            it.completed = preferences.getBoolean("${it.name}_completed", false)
+        }
         val gameChooserView = Intent(v.context, ChooseCategory::class.java)
         startActivity(gameChooserView)
     }
